9a467d39dd8b4511111b6305acb7d16057b23d80
fvoidxf <30986764+fvoidxf@users.noreply.github.com>
2026-08-26 19:35:49 +0300

    Delete watchdog directory

--- watchdog/filesize.h
+++
@@ -1,8 +0,0 @@
-/*
-* created by fv01dxf@gmail.com
-* General Public License v3 
-*         2019
-*/
-#pragma once
-
-unsigned long long FileSize(const std::string& filename);
--- watchdog/getfiledata.h
+++
@@ -1,9 +0,0 @@
-/*
-* created by fv01dxf@gmail.com
-* General Public License v3 
-*         2019
-*/
-
-#pragma once
-
-bool GetFileData(const std::string& filename, boost::shared_array<char>& data, unsigned long long filesize);
